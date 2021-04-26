5331108c99fb050b4dda4ed1a66710e718ccd43e
europa1610 <afzalahmad0203@gmail.com>
2021-04-26 11:08:19 +0000

    Segmented code into modules

--- detection.py
+++ detection.py
@@ -11,328 +11,14 @@ import argparse
 from time import time
 from torch.optim import lr_scheduler
 from utils.dsetparser import parse_dset_config
+from models.wR2 import wR2
+from dataloader.loader import ChaLocDataLoader
+from trainer.trainer import train_model 
 
-USE_WANDB = False
+USE_WANDB = True
 if USE_WANDB:
     import wandb
-    wandb.init(project='alpr', entity='afzal', name='detection_skeleton_0')
-
-#Courtesy of https://github.com/detectRecog/CCPD/
-class ChaLocDataLoader(Dataset):
-    def __init__(self, img_dir, imgSize, is_transform=None):
-        with open(img_dir, 'r') as file:
-            self.img_paths = file.readlines()
-
-        #self.img_dir = img_dir
-        #self.img_paths = []
-        #for i in range(len(img_dir)):
-        #    self.img_paths += [el for el in paths.list_images(img_dir[i])]
-        self.img_size = imgSize
-        self.is_transform = is_transform
-
-    def __len__(self):
-        return len(self.img_paths)
-
-    def __getitem__(self, index):
-        img_name = self.img_paths[index]
-        img_name = os.path.join('CCPD2019', img_name)
-        img = cv2.imread(img_name.strip())
-        if img is None:
-            print('Could\'nt read image at path {}'.format(img_name))
-            print('Later, Loser...')
-            exit(0)
-        resizedImage = cv2.resize(img, self.img_size)
-        resizedImage = np.transpose(resizedImage, (2, 0, 1))
-
-        iname = img_name.rsplit('/', 1)[-1].rsplit('.', 1)[0].split('-')
-        [leftUp, rightDown] = [[int(eel) for eel in el.split('&')] for el in iname[2].split('_')]
-        #---Uncomment to generate image with bounding box
-        #cv2.rectangle(img, (leftUp[0], leftUp[1]), (rightDown[0], rightDown[1]), (255, 0, 0), 1) 
-        #cv2.imwrite('res1.jpg', img)
-        #---
-
-        ori_w, ori_h = float(img.shape[1]), float(img.shape[0])
-        assert img.shape[0] == 1160
-        new_labels = [(leftUp[0] + rightDown[0])/(2*ori_w), (leftUp[1] + rightDown[1])/(2*ori_h), (rightDown[0]-leftUp[0])/ori_w, (rightDown[1]-leftUp[1])/ori_h]
-
-        resizedImage = resizedImage.astype('float32')
-        resizedImage /= 255.0
-
-        return resizedImage, new_labels
-
-
-class wR2(nn.Module):
-    def __init__(self, num_classes=1000):
-        super(wR2, self).__init__()
-        hidden1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=48, kernel_size=5, padding=2, stride=2),
-            nn.BatchNorm2d(num_features=48),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden2 = nn.Sequential(
-            nn.Conv2d(in_channels=48, out_channels=64, kernel_size=5, padding=2),
-            nn.BatchNorm2d(num_features=64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden3 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2),
-            nn.BatchNorm2d(num_features=128),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden4 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=160, kernel_size=5, padding=2),
-            nn.BatchNorm2d(num_features=160),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden5 = nn.Sequential(
-            nn.Conv2d(in_channels=160, out_channels=192, kernel_size=5, padding=2),
-            nn.BatchNorm2d(num_features=192),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden6 = nn.Sequential(
-            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2),
-            nn.BatchNorm2d(num_features=192),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden7 = nn.Sequential(
-            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2),
-            nn.BatchNorm2d(num_features=192),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden8 = nn.Sequential(
-            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=5, padding=2),
-            nn.BatchNorm2d(num_features=192),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden9 = nn.Sequential(
-            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=192),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
-            nn.Dropout(0.2)
-        )
-        hidden10 = nn.Sequential(
-            nn.Conv2d(in_channels=192, out_channels=192, kernel_size=3, padding=1),
-            nn.BatchNorm2d(num_features=192),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=1, padding=1),
-            nn.Dropout(0.2)
-        )
-        self.features = nn.Sequential(
-            hidden1,
-            hidden2,
-            hidden3,
-            hidden4,
-            hidden5,
-            hidden6,
-            hidden7,
-            hidden8,
-            hidden9,
-            hidden10
-        )
-        self.classifier = nn.Sequential(
-            nn.Linear(23232, 100),
-            # nn.ReLU(inplace=True),
-            nn.Linear(100, 100),
-            # nn.ReLU(inplace=True),
-            nn.Linear(100, num_classes),
-        )
-
-    def forward(self, x):
-        x1 = self.features(x)
-        x11 = x1.view(x1.size(0), -1)
-        x = self.classifier(x11)
-        return x
-
-def draw_bbox(image, bbox, color):  #Input/Output in (C, H, W), 0-1 range
-    image_cp = np.copy(image)
-    h, w = image_cp.shape[1], image_cp.shape[2]
-    image_cp = np.float32(channels_last(image_cp))
-    topleft_x = int(bbox[0]*w - bbox[2]*w/2.)
-    topleft_y = int(bbox[1]*h - bbox[3]*h/2.)
-    botright_x = int(bbox[0]*w + bbox[2]*w/2.)
-    botright_y = int(bbox[1]*h + bbox[3]*h/2.)
-    if color == 'g':
-        cl = (0, 255, 0)
-    elif color == 'r':
-        cl = (0, 0, 255)
-    else:
-        cl = (0, 0, 0)
-    cv2.rectangle(image_cp, (topleft_x, topleft_y), (botright_x, botright_y), cl, 1) 
-    return channels_first(image_cp)
-
-def channels_last(image):
-    return np.transpose(image, (1, 2, 0))
-
-def channels_first(image):
-    return np.transpose(image, (2, 0, 1))
-
-def write_image(image, filename): #Input in (C, H, W), 0-1 range
-    image_cp = np.copy(image)
-    image_cp = np.float32(channels_last(image_cp))
-    print('Writing Image...')
-    cv2.imwrite(filename, image_cp*255.0)
-    return
-
-def retrieve_img(image):
-    cp = np.copy(image)
-    cp = channels_last(cp)
-    resized = cv2.resize(cp, (720, 1160))
-    cp = channels_first(resized)
-    return cp
-
-def train_model(model, criterion, optimizer, lrScheduler, trainloader, evalloader, batchSize, num_epochs=300):
-    for epoch in range(1, num_epochs):
-        model.train()
-        print(f'Starting Epoch {epoch}')
-        lossAver = []
-        lrScheduler.step()
-        start = time()
-        dset_len = len(trainloader)
-        correct_pred = 0
-        for i, (XI, YI) in enumerate(trainloader):
-            # print('%s/%s %s' % (i, times, time()-start))
-            YI = np.array([el.numpy() for el in YI]).T
-            x = Variable(XI.cuda(0))
-            y = Variable(torch.FloatTensor(YI).cuda(0), requires_grad=False)
-            # Forward pass: Compute predicted y by passing x to the model
-            y_pred = model(x)
-
-            # Compute and print loss
-            loss = 0.0
-            if len(y_pred) == batchSize:
-                loss += 0.8 * nn.L1Loss().cuda()(y_pred[:,:2], y[:,:2]) #Penalizing more on box center coordinates
-                loss += 0.2 * nn.L1Loss().cuda()(y_pred[:,2:], y[:,2:])
-                lossAver.append(loss.item())
-
-                # Zero gradients, perform a backward pass, and update the weights.
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                batch_iou = IoU(YI*480, 480*y_pred.cpu().detach().numpy())
-                bin_correct = (batch_iou >= 0.7)
-                correct_pred += np.sum(bin_correct)
-
-            if (i*batchSize)%4999 == 1:
-                #old_img = retrieve_img(XI[0])
-                #old_img = draw_bbox(old_img, YI[0],'g')
-                #old_img = draw_bbox(old_img, y_pred[0],'r')
-                #iou_val = IoU(YI[0:1]*480, 480*y_pred[0:1].cpu().detach().numpy())
-                #write_image(old_img, str(iou_val[0])+'.jpg')
-                torch.save(model.state_dict(), 'trained_models/checkpoints/save_ckpt.pth')
-                if USE_WANDB:
-                #    wandb.log({'Sample Detections': [wandb.Image(channels_last(old_img*255.0), caption='iou='+str(iou_val)+'.jpg')]})
-                    wandb.save('trained_models/checkpoints/save_ckpt.pth')
-
-            if i % 500 == 0:
-                curloss = sum(lossAver)/len(lossAver)
-                curacc = correct_pred/((i+1)*batchSize)
-                if USE_WANDB:
-                    wandb.log({'Current Training Loss':curloss}, step=int((epoch-1)*dset_len/500+i/500))
-                    wandb.log({'Current Training Acc':curacc}, step=int((epoch-1)*dset_len/500+i/500))
-                print('Epoch {}, Processed: {}, Time: {}, Loss: {}, Acc: {}'.format(epoch, i*batchSize, time()-start, curloss, curacc))
-        print ('Epoch %s Trained, Loss: %s, Accuracy: %s, Time: %s\n' % (epoch, sum(lossAver) / len(lossAver), correct_pred/(dset_len*batchSize), time()-start))
-        if USE_WANDB:
-            wandb.log({'Epoch Train Loss': sum(lossAver)/len(lossAver)}, step = epoch)
-            wandb.log({'Epoch Train Accuracy': correct_pred/(dset_len*batchSize)}, step = epoch)
-            wandb.save('trained_models/epochs/save_epoch' + str(epoch) + '.pth')
-        torch.save(model.state_dict(), 'trained_models/epochs/save_epoch' + str(epoch) + '.pth')
-        #Begin Eval here
-        val_model(model, evalloader, batchSize, epoch) 
-    return 
-
-def val_model(model, evalloader, batchSize, epoch):
-    model.eval()
-    lossAver = []
-    start = time()
-    dset_len = len(evalloader)
-    correct_pred = 0
-    for i, (XI, YI) in enumerate(evalloader):
-        YI = np.array([el.numpy() for el in YI]).T
-        x = Variable(XI.cuda(0))
-        y = Variable(torch.FloatTensor(YI).cuda(0), requires_grad=False)
-        # Forward pass: Compute predicted y by passing x to the model
-        y_pred = model(x)
-
-        # Compute and print loss
-        loss = 0.0
-        loss += 0.8 * nn.L1Loss().cuda()(y_pred[:,:2], y[:,:2]) #Penalizing more on box center coordinates
-        loss += 0.2 * nn.L1Loss().cuda()(y_pred[:,2:], y[:,2:])
-        lossAver.append(loss.item())
-
-        batch_iou = IoU(YI*480, 480*y_pred.cpu().detach().numpy())
-        #print('batch_iou:')
-        #print(batch_iou)
-        bin_correct = (batch_iou >= 0.7)
-        #print('Correct/Incorrect:')
-        #print(bin_correct)
-        correct_pred += np.sum(bin_correct)
-        #print('Number correct so far: {}'.format(correct_pred))
-
-        if (i*batchSize)%10000 == 0:
-            #write_image(old_img, str(iou_val[0])+'.jpg')
-            #print(iou_val)
-            if USE_WANDB:
-                old_img = retrieve_img(XI[0])
-                old_img = draw_bbox(old_img, YI[0],'g')
-                old_img = draw_bbox(old_img, y_pred[0],'r')
-                iou_val = batch_iou[0] 
-                wandb.log({'Sample Test Detections': [wandb.Image(channels_last(old_img*255.0), caption='iou='+str(iou_val)+'.jpg')]}, commit=False) #step=int((epoch-1)*dset_len*batchSize/10000 + i*batchSize/10000))
-
-        if i % 500 == 0:
-            curloss = sum(lossAver)/len(lossAver)
-            print('Evaluating Epoch: {}, Processed: {}, Time: {}, Loss: {}, Accuracy: {}'.format(epoch, i*batchSize, time()-start, curloss, correct_pred/((i+1)*batchSize)))
-    print ('Epoch %s Evaluated, Loss: %s, Accuracy: %s, Time Elapsed: %s\n' % (epoch, sum(lossAver) / len(lossAver), correct_pred/((i+1)*batchSize), time()-start))
-    if USE_WANDB:
-        wandb.log({'Eval Epoch Loss': sum(lossAver)/len(lossAver)}, step=epoch)
-        wandb.log({'Eval Epoch Accuracy': correct_pred/((i+1)*batchSize)}, step=epoch)
-    return
-
-def IoU(boxa, boxb):
-    boxA = np.zeros(boxa.shape)
-    boxB = np.zeros(boxb.shape)
-    boxA[:,:2] = boxa[:,:2] - boxa[:,2:]/2.
-    boxA[:,2:] = boxa[:,:2] + boxa[:,2:]/2.
-    boxB[:,:2] = boxb[:,:2] - boxb[:,2:]/2.
-    boxB[:,2:] = boxb[:,:2] + boxb[:,2:]/2.
-
-    # determine the (x, y)-coordinates of the intersection rectangle
-    xA = np.maximum(boxA[:,0], boxB[:,0])
-    yA = np.maximum(boxA[:,1], boxB[:,1])
-    xB = np.minimum(boxA[:,2], boxB[:,2])
-    yB = np.minimum(boxA[:,3], boxB[:,3])
-
-    # compute the area of intersection rectangle
-    interArea = np.maximum(0, xB - xA + 1) * np.maximum(0, yB - yA + 1)
-
-    # compute the area of both the prediction and ground-truth
-    # rectangles
-    boxAArea = (boxA[:,2] - boxA[:,0] + 1) * (boxA[:,3] - boxA[:,1] + 1)
-    boxBArea = (boxB[:,2] - boxB[:,0] + 1) * (boxB[:,3] - boxB[:,1] + 1)
-    
-    # compute the intersection over union by taking the intersection
-    # area and dividing it by the sum of prediction + ground-truth
-    # areas - the interesection area
-    iou = np.divide(interArea, boxAArea + boxBArea - interArea)
-
-    # return the intersection over union value
-    return iou
+    wandb.init(project='alpr', entity='afzal', name='detection_reluo')
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--dsetconf", default='dset_config/config.ccpd',
@@ -359,6 +45,7 @@ def main():
         config.epochs = epochs
         config.lr = lr
         config.momentum = momentum
+        wandb.save('./*.py')
 
     model = wR2(numClasses)
     #model = torch.nn.DataParallel(model, device_ids=range(torch.cuda.device_count())) # This piece of shit hangs 
@@ -368,7 +55,7 @@ def main():
             #since working on a remote node. Got no choice but to work with a single GPU. 
             #In summary, as Linus Torvalds would say: FUCK YOU NVIDIA
     model = model.cuda()
-    criterion = nn.MSELoss()
+    criterion = nn.MSELoss().cuda()
     optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
     lrScheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
     dset_conf = parse_dset_config(args['dsetconf'])
@@ -381,7 +68,7 @@ def main():
     valdst = ChaLocDataLoader(valloc, imgSize)
     evalloader = DataLoader(valdst, batch_size=batchSize, shuffle=False, num_workers=4)
     print('Starting Training...')
-    model_conv = train_model(model, criterion, optimizer, lrScheduler, trainloader, evalloader, batchSize, num_epochs=epochs)
+    model_conv = train_model(model, criterion, optimizer, lrScheduler, trainloader, evalloader, batchSize, num_epochs=epochs, USE_WANDB=USE_WANDB)
 
 if __name__=='__main__':
     main()
---
+++ trainer/trainer.py
@@ -0,0 +1,116 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import wandb
+import numpy as np
+from time import time
+from utils.utils import IoU
+
+def train_model(model, criterion, optimizer, lrScheduler, trainloader, evalloader, batchSize, num_epochs=300, USE_WANDB=False):
+    for epoch in range(1, num_epochs):
+        model.train()
+        print(f'Starting Epoch {epoch}')
+        lossAver = []
+        start = time()
+        dset_len = len(trainloader)
+        correct_pred = 0
+        for i, (XI, YI) in enumerate(trainloader):
+            # print('%s/%s %s' % (i, times, time()-start))
+            YI = np.array([el.numpy() for el in YI]).T
+            x = Variable(XI.cuda(0))
+            y = Variable(torch.FloatTensor(YI).cuda(0), requires_grad=False)
+            # Forward pass: Compute predicted y by passing x to the model
+            y_pred = model(x)
+
+            # Compute and print loss
+            loss = 0.0
+            if len(y_pred) == batchSize:
+                loss += 0.8 * criterion(y_pred[:,:2], y[:,:2]) #Penalizing more on box center coordinates
+                loss += 0.2 * criterion(y_pred[:,2:], y[:,2:])
+                lossAver.append(loss.item())
+
+                # Zero gradients, perform a backward pass, and update the weights.
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                batch_iou = IoU(YI*480, 480*y_pred.cpu().detach().numpy())
+                bin_correct = (batch_iou >= 0.7)
+                correct_pred += np.sum(bin_correct)
+
+            if (i*batchSize)%4999 == 1:
+                #old_img = retrieve_img(XI[0])
+                #old_img = draw_bbox(old_img, YI[0],'g')
+                #old_img = draw_bbox(old_img, y_pred[0],'r')
+                #iou_val = IoU(YI[0:1]*480, 480*y_pred[0:1].cpu().detach().numpy())
+                #write_image(old_img, str(iou_val[0])+'.jpg')
+                torch.save(model.state_dict(), 'trained_models/checkpoints/save_ckpt.pth')
+                if USE_WANDB:
+                #    wandb.log({'Sample Detections': [wandb.Image(channels_last(old_img*255.0), caption='iou='+str(iou_val)+'.jpg')]})
+                    wandb.save('trained_models/checkpoints/save_ckpt.pth')
+
+            if i % 500 == 0:
+                curloss = sum(lossAver)/len(lossAver)
+                curacc = correct_pred/((i+1)*batchSize)
+                if USE_WANDB:
+                    wandb.log({'Current Training Loss':curloss}, step=int((epoch-1)*dset_len/500+i/500))
+                    wandb.log({'Current Training Acc':curacc}, step=int((epoch-1)*dset_len/500+i/500))
+                print('Epoch {}, Processed: {}, Time: {}, Loss: {}, Acc: {}'.format(epoch, i*batchSize, time()-start, curloss, curacc))
+        print ('Epoch %s Trained, Loss: %s, Accuracy: %s, Time: %s\n' % (epoch, sum(lossAver) / len(lossAver), correct_pred/(dset_len*batchSize), time()-start))
+        lrScheduler.step()
+        if USE_WANDB:
+            wandb.log({'Epoch Train Loss': sum(lossAver)/len(lossAver)}, step = epoch)
+            wandb.log({'Epoch Train Accuracy': correct_pred/(dset_len*batchSize)}, step = epoch)
+            wandb.save('trained_models/epochs/save_epoch' + str(epoch) + '.pth')
+        torch.save(model.state_dict(), 'trained_models/epochs/save_epoch' + str(epoch) + '.pth')
+        #Begin Eval here
+        val_model(model, evalloader, batchSize, epoch, USE_WANDB) 
+    return 
+
+def val_model(model, evalloader, batchSize, epoch, USE_WANDB):
+    model.eval()
+    lossAver = []
+    start = time()
+    dset_len = len(evalloader)
+    correct_pred = 0
+    for i, (XI, YI) in enumerate(evalloader):
+        YI = np.array([el.numpy() for el in YI]).T
+        x = Variable(XI.cuda(0))
+        y = Variable(torch.FloatTensor(YI).cuda(0), requires_grad=False)
+        # Forward pass: Compute predicted y by passing x to the model
+        y_pred = model(x)
+
+        # Compute and print loss
+        loss = 0.0
+        loss += 0.8 * nn.L1Loss().cuda()(y_pred[:,:2], y[:,:2]) #Penalizing more on box center coordinates
+        loss += 0.2 * nn.L1Loss().cuda()(y_pred[:,2:], y[:,2:])
+        lossAver.append(loss.item())
+
+        batch_iou = IoU(YI*480, 480*y_pred.cpu().detach().numpy())
+        #print('batch_iou:')
+        #print(batch_iou)
+        bin_correct = (batch_iou >= 0.7)
+        #print('Correct/Incorrect:')
+        #print(bin_correct)
+        correct_pred += np.sum(bin_correct)
+        #print('Number correct so far: {}'.format(correct_pred))
+
+        if (i*batchSize)%10000 == 0:
+            #write_image(old_img, str(iou_val[0])+'.jpg')
+            #print(iou_val)
+            if USE_WANDB:
+                old_img = retrieve_img(XI[0])
+                old_img = draw_bbox(old_img, YI[0],'g')
+                old_img = draw_bbox(old_img, y_pred[0],'r')
+                iou_val = batch_iou[0] 
+                wandb.log({'Sample Test Detections': [wandb.Image(channels_last(old_img*255.0), caption='iou='+str(iou_val)+'.jpg')]}, commit=False) #step=int((epoch-1)*dset_len*batchSize/10000 + i*batchSize/10000))
+
+        if i % 500 == 0:
+            curloss = sum(lossAver)/len(lossAver)
+            print('Evaluating Epoch: {}, Processed: {}, Time: {}, Loss: {}, Accuracy: {}'.format(epoch, i*batchSize, time()-start, curloss, correct_pred/((i+1)*batchSize)))
+    print ('Epoch %s Evaluated, Loss: %s, Accuracy: %s, Time Elapsed: %s\n' % (epoch, sum(lossAver) / len(lossAver), correct_pred/((i+1)*batchSize), time()-start))
+    if USE_WANDB:
+        wandb.log({'Eval Epoch Loss': sum(lossAver)/len(lossAver)}, step=epoch)
+        wandb.log({'Eval Epoch Accuracy': correct_pred/((i+1)*batchSize)}, step=epoch)
+    return
+
+
